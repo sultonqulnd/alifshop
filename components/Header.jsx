38dff078c14e267ff5278f05374057e057555798
sultonqulnd <sultonqulnd@gmail.com>
2024-06-26 10:01:20 +0500

    project setup

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="max-w-1160 px-5 mx-auto">
+      <img src="logo.svg" alt="" />
+    </div>
+  );
+};
+
+export default Header;
